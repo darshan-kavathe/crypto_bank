483d849bbb86a0f339de47bfe8dda9401e14f472
dck1135 <dck1135@rit.edu>
2018-04-04 21:35:19 -0400

    initial code

---
+++ src2/rng.cpp
@@ -0,0 +1,16 @@
+#include "../src2/rng.h"
+
+using std::random_device;
+
+RNG::RNG() :
+        RNG{random_device{}()} {
+}
+
+RNG::RNG(unsigned int seed) :
+        gen_{seed},
+        dis_{} {
+}
+
+unsigned long long RNG::rand() {
+    return dis_(gen_);
+}
